76026242a0add7bd0d4c293942b7bf71bf0bf8be
Strobolight1 <strobolight1@gmail.com>
2026-02-12 22:04:06 +0900

    Delete data_folder directory

--- data_folder/Trim1.ps1
+++
@@ -1,8 +0,0 @@
-﻿Get-ChildItem -Filter *.html | ForEach-Object {
-    $oldName = $_.Name
-    if ($oldName -match "(_ネットワークスペシャリスト試験.*)") {
-        $newName = $Matches[1]
-        Rename-Item -Path $_.FullName -NewName $newName
-        Write-Host "✅ Renamed: $oldName → $newName"
-    }
-}
